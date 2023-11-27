4d71d754212f73900636d09d5c5ca41579b73387
AAYUSH DOGRA <56511092+aayushdogra@users.noreply.github.com>
2023-11-27 18:59:52 +0530

    DB connection updated

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb+srv://admin-aayush:chutiya18@cluster0.ydavvcl.mongodb.net/?retryWrites=true&w=majority")
 
 const app = express();
 
@@ -13,7 +15,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 // Defining schema
 const itemsSchema = new mongoose.Schema({
@@ -141,6 +142,11 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server is running at Port 3000.");
 });
